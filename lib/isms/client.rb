dd6a537594702c14e2ed4cb65572eee494dbf853
Jonathan ES Lin <ernsheong@gmail.com>
2015-03-05 16:34:29 +0800

    Initial send sms implementation

--- lib/isms/client.rb
+++ lib/isms/client.rb
@@ -1,8 +1,47 @@
+require 'isms/configuration'
+
 module ISMS
   class Client
+    SEND_SMS_URL = '/isms_send.php'
+
     def initialize
       # Check for username and password
       raise RuntimeError, "Username and password not provided!" unless ISMS.username && ISMS.password
+    end
+
+    #
+    # Returns Hash of :code and :description
+    # code has the error code number
+    # description is the error description
+    #
+    def send_sms(message_body, phone_no, type = 1)
+      request_body = {}
+      request_body[:un] = ISMS.username
+      request_body[:pwd] = ISMS.password
+      request_body[:dstno] = phone_no
+      request_body[:msg] = message_body
+      request_body[:type] = type
+
+      faraday_response = connection.post do |request|
+        request.url SEND_SMS_URL
+        request.body = request_body
+      end
+
+      parse_response(faraday_response.body)
+    end
+
+    private
+
+    def connection
+      @connection ||= Faraday.new(url: ISMS.endpoint_url_with_protocol)
+    end
+
+    def parse_response(body)
+      result_array = body.split('=')
+      {
+          code: result_array.first.strip.to_i,
+          description: result_array.last.strip
+      }
     end
   end
 end
--- lib/isms/configuration.rb
+++ lib/isms/configuration.rb
@@ -3,10 +3,11 @@ require 'isms/version'
 module ISMS
   module Configuration
     DEFAULT_PROTOCOL = 'http'
+    DEFAULT_ENDPOINT = 'www.isms.com.my'
     DEFAULT_USERNAME = nil
     DEFAULT_PASSWORD = nil
 
-    attr_accessor :username, :password, :protocol
+    attr_accessor :username, :password, :protocol, :endpoint
 
     # When extended, call reset to set variable values to defaults
     def self.extended(mod)
@@ -17,11 +18,15 @@ module ISMS
       self.username = DEFAULT_USERNAME
       self.password = DEFAULT_PASSWORD
       self.protocol = DEFAULT_PROTOCOL
+      self.endpoint = DEFAULT_ENDPOINT
+    end
+
+    def endpoint_url_with_protocol
+      "#{protocol}://#{endpoint}"
     end
 
     def configure
       yield self
     end
-
   end
 end
